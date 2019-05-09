1bd4ccac6a805f121b58b05ede8fa10e9c5944a4
Joshua Loran <joshua.loran@gmail.com>
2019-05-09 12:53:25 -0700

    added game seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Game.create!(name: "tictactoe", array: [{id: 0, user_emoji: ''},
+                                        {id: 0, user_emoji: ''},
+                                        {id: 0, user_emoji: ''},
+                                        {id: 0, user_emoji: ''},
+                                        {id: 0, user_emoji: ''},
+                                        {id: 0, user_emoji: ''},
+                                        {id: 0, user_emoji: ''},
+                                        {id: 0, user_emoji: ''},
+                                        {id: 0, user_emoji: ''}
+                                        ])
